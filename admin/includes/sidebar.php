f72814d1af7d39681149859e73787c27a05d8b94
pspcsc <156990752+pspcsc@users.noreply.github.com>
2026-08-18 22:52:00 +0530

    Link admin sidebar to CRUD manager

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 ?>
 <div class="list-group list-group-flush rounded-0">
     <a class="list-group-item list-group-item-action py-3" href="dashboard.php"><i class="bi bi-speedometer2 me-2"></i>Dashboard</a>
-    <a class="list-group-item list-group-item-action py-3" href="services.php"><i class="bi bi-grid me-2"></i>Services</a>
-    <a class="list-group-item list-group-item-action py-3" href="projects.php"><i class="bi bi-briefcase me-2"></i>Projects</a>
+    <a class="list-group-item list-group-item-action py-3" href="manage.php?entity=services"><i class="bi bi-grid me-2"></i>Services</a>
+    <a class="list-group-item list-group-item-action py-3" href="manage.php?entity=projects"><i class="bi bi-briefcase me-2"></i>Projects</a>
+    <a class="list-group-item list-group-item-action py-3" href="manage.php?entity=blogs"><i class="bi bi-journal-text me-2"></i>Blogs</a>
+    <a class="list-group-item list-group-item-action py-3" href="manage.php?entity=news"><i class="bi bi-newspaper me-2"></i>News</a>
+    <a class="list-group-item list-group-item-action py-3" href="manage.php?entity=testimonials"><i class="bi bi-chat-quote me-2"></i>Testimonials</a>
+    <a class="list-group-item list-group-item-action py-3" href="manage.php?entity=gallery"><i class="bi bi-images me-2"></i>Gallery</a>
+    <a class="list-group-item list-group-item-action py-3" href="manage.php?entity=hero-slides"><i class="bi bi-sliders me-2"></i>Hero Slides</a>
     <a class="list-group-item list-group-item-action py-3" href="pages.php"><i class="bi bi-file-earmark-text me-2"></i>Pages</a>
     <a class="list-group-item list-group-item-action py-3" href="enquiries.php"><i class="bi bi-inbox me-2"></i>Enquiries</a>
     <a class="list-group-item list-group-item-action py-3" href="settings.php"><i class="bi bi-gear me-2"></i>Settings</a>
