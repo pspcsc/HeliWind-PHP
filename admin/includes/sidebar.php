fe5378119e22572992d6a8aca7a843f2f4c93768
pspcsc <156990752+pspcsc@users.noreply.github.com>
2026-08-18 22:47:00 +0530

    Add admin sidebar

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+?>
+<div class="list-group list-group-flush rounded-0">
+    <a class="list-group-item list-group-item-action py-3" href="dashboard.php"><i class="bi bi-speedometer2 me-2"></i>Dashboard</a>
+    <a class="list-group-item list-group-item-action py-3" href="services.php"><i class="bi bi-grid me-2"></i>Services</a>
+    <a class="list-group-item list-group-item-action py-3" href="projects.php"><i class="bi bi-briefcase me-2"></i>Projects</a>
+    <a class="list-group-item list-group-item-action py-3" href="pages.php"><i class="bi bi-file-earmark-text me-2"></i>Pages</a>
+    <a class="list-group-item list-group-item-action py-3" href="enquiries.php"><i class="bi bi-inbox me-2"></i>Enquiries</a>
+    <a class="list-group-item list-group-item-action py-3" href="settings.php"><i class="bi bi-gear me-2"></i>Settings</a>
+</div>
